056b26fc25aa5af099c3357e6874809c05efc06d
MLTurner <mlturne3@ncsu.edu>
2022-04-04 21:16:40 -0400

    attempted to make fetch function

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+console.log(process.env.PATH)
 const spawn = require('child_process').spawn;
 const liveServer = require('live-server')
 const sleep = time => new Promise(resolve => setTimeout(resolve,time))
 async function main(){
-    spawn('npx', ['json-server', '--watch', '--port', '9001', 'db.json' ], { stdio: 'inherit'});
+    spawn('npx', ['json-server', '--watch', '--port', '9001', 'db.json' ], { stdio: 'inherit', shell: true});
     await sleep(1000) // <-- Wait one second, so that the json-server has started by the time the live-server opens.
     liveServer.start({
         port: 9000,
@@ -11,3 +12,6 @@ async function main(){
     })
 }
 main()
+async function getCounter(){
+    let response = await fetch('')
+}
